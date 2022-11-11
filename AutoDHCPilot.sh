18bf0f1bc992f21c9a6092344dd7bbe71363f321
Anand Kore <103954920+TheOneOh1@users.noreply.github.com>
2022-11-11 15:32:18 +0530

    Create AutoDHCPilot.sh

---
+++ AutoDHCPilot.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+function main(){
+
+    echo -e "\n*** DHCP Server Configuration ***\n"
+
+    read -p "Enter Domain name : " od
+
+
+    echo -e "option domain-name \"$od\";" > /etc/dhcp/dhcpd.conf
+    echo -e "\ndefault-lease-time 3600; \nmax-lease-time 7200; \nauthoritative;" >> /etc/dhcp/dhcpd.conf
+    echo -e "\nddns-update-style none;" >> /etc/dhcp/dhcpd.conf
+
+
+    ip=$(ifconfig ens33 | grep "inet " | awk -F" " '{print ""$2}')
+    netmask=$(ifconfig ens33 | grep "inet " | awk -F" " '{print ""$4}')
+
+    read -p "Enter Subnet : " sub
+    read -p "Enter Option Routers : " option_r
+    read -p "IP Pool starting from : " ip_start
+    read -p "IP Pool ending at : " ip_end
+
+    echo -e "\nsubnet $sub netmask $netmask { \noption routers	$option_r; \noption subnet-mask		$netmask; \noption domain-search	\"$od\";\noption domain-name-servers	$option_r; \nrange $ip_start $ip_end; \n}" >> /etc/dhcp/dhcpd.conf
+
+    echo -e "\n\nInput has been registered"
+    start=$(systemctl restart isc-dhcp-server)
+    enable=$(systemctl enable isc-dhcp-server)
+
+    echo -e "\n $start $enable DHCP has been started and has been enabled successfully !!!"
+
+    chk_ip
+
+}
+
+function chk_ip(){
+
+    while true ;
+    do
+            read -p "Would like to check IP allocation (y/n) : " ch
+
+            case "$ch" in
+                    "y" | "Y")
+                            #echo -e "You have selected Yes"
+                            lease=$(cat /var/lib/dhcp/dhcpd.leases | grep -iE "lease" | tail -1 | awk -F" " '{print ""$2}')
+                            echo -e "\n $lease has been assigned"
+                            exit 0
+                            ;;
+
+                    "n" | "N")
+                            exit 0
+                            ;;
+
+                    *)
+                            echo -e "Invalid input please select (y/n)"
+                            ;;
+            esac
+    done
+
+}
+
+main
